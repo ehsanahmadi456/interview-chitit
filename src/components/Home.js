0fceae770f3cc685f44bd816771278314a7ac3e4
ehsan ahmadi <ehsanahmadi8338@gmail.com>
2022-07-05 22:52:29 +0430

    add navbar to project

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Components
+import NavBar from './NavBar';
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// styles
+import styles from "./NavBar.module.css";
+
+// Icon
+import login from "../assets/icon/login.svg";
+import download from "../assets/icon/download.svg";
+import plus from "../assets/icon/plus.svg";
+import arrowBottom from "../assets/icon/arrowBottom.svg";
+
+const NavBar = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.navbarLeft}>
+                <button><p>ورود/ثبت نام</p> <img src={login} alt="login icon" /></button>
+            </div>
+            <div className={styles.navbarRight}>
+                <ul>
+                    <li><button>آگهی بزن </button><img src={plus} alt="plus" /></li>
+                    <li><p>دانلود اپلیکیشن </p><img src={download} alt="download" /></li>
+                    <li><p>دانستنی های استخدامی</p></li>
+                    <li><p>کار در منزل</p></li>
+                    <li> <img src={arrowBottom} alt="arrowBottom" /><p>کار فرمایان</p></li>
+                    <li> <img src={arrowBottom} alt="arrowBottom" /><p>کار جویان</p></li>
+                    <li><p>لیست آگهی ها</p></li>
+                    <li><p>صفحه نخست</p></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
